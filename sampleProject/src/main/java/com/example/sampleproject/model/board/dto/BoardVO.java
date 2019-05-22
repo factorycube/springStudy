f02c8d7e750ec42b0b01d045ae2ac6617df9e2a1
gwangyu1020 <gwangyu1020@gmail.com>
2019-05-22 22:22:05 +0900

    11. 게시판 만들기 연습 (글쓰기, 수정, 삭제 회원권한부여)

--- sampleProject/src/main/java/com/example/sampleproject/model/board/dto/BoardVO.java
+++ sampleProject/src/main/java/com/example/sampleproject/model/board/dto/BoardVO.java
@@ -9,6 +9,7 @@ public class BoardVO {
     private String writer;      // 게시글 작성자
     private Date regdate;       // 게시글 작성일자 util.Date
     private int viewcnt;        // 게시글 조회수
+    private String userName;	// 게시글 회원이름 = 게시글 작성자
 	
     // Getter/Setter
     public int getBno() {
@@ -47,10 +48,16 @@ public class BoardVO {
 	public void setViewcnt(int viewcnt) {
 		this.viewcnt = viewcnt;
 	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 	// toString()
 	@Override
 	public String toString() {
 		return "BoardVO [bno=" + bno + ", title=" + title + ", content=" + content + ", writer=" + writer + ", regdate="
-				+ regdate + ", viewcnt=" + viewcnt + "]";
+				+ regdate + ", viewcnt=" + viewcnt + ", userName=" + userName + "]";
 	}
 }
